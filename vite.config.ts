53b422902766b49d013f4cff84d8b4b9142e96f8
Natto <thisis@natalies.email>
2026-06-08 18:26:15 -0700

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: "/main/",
+  base: "/",
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
