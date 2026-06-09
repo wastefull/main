4cdebdf55316a468e7bb2903064bf370c6dc734e
Natalie Orsi <nat@wastefull.org>
2026-06-08 22:41:36 -0700

    Removed unneeded baggage from original Figma project

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-function figmaAssetResolver() {
-  return {
-    name: "figma-asset-resolver",
-    resolveId(id) {
-      if (id.startsWith("figma:asset/")) {
-        const filename = id.replace("figma:asset/", "");
-        return path.resolve(__dirname, "src/assets", filename);
-      }
-    },
-  };
-}
-
 export default defineConfig({
   base: "/",
-  plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       // Alias @ to the src directory
